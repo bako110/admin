cbc6dc7c928246467a80a9871fe94f9901c25784
bako110 <bakorobert2000@gmail.com>
2025-08-20 07:21:12 +0200

    commit important

--- scripts/login.js
+++ scripts/login.js
@@ -1,12 +1,10 @@
-// scripts/login.js
-
 let token = null;
 
 function login() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    fetch(`${API_URL}/api/login`, { // API_URL est défini dans config.js
+    fetch(`${API_URL}/api/login`, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({ username, password })
@@ -31,9 +29,12 @@ function loadAdminContent() {
         return res.json();
     })
     .then(data => {
+        // Masquer le formulaire de login
         document.getElementById('loginDiv').style.display = 'none';
+        // Afficher la div admin
         document.getElementById('adminDiv').style.display = 'block';
-        document.getElementById('adminContent').innerText = JSON.stringify(data, null, 2);
+        // Afficher le message avec le username
+        document.getElementById('adminContent').innerText = `Vous êtes connecté en tant que : ${data.user.username}`;
     })
     .catch(err => alert(err.message));
 }
